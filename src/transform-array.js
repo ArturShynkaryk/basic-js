a5d5dc9be302c827aaaa9c1f901ba6be7b95cff3
Artur Shynkaryk <mun9je@gmail.com>
2023-04-13 20:46:22 +0300

    feat: add new tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -18,29 +18,29 @@ function transform(arr) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   }
 
-  const transformedArr = [];
+  const transformer = [];
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === '--discard-next') {
       i++;
     } else if (arr[i] === '--discard-prev') {
       if (i > 0 && arr[i - 2] !== '--discard-next') {
-        transformedArr.pop();
+        transformer.pop();
       }
     } else if (arr[i] === '--double-next') {
       if (i < arr.length - 1) {
-        transformedArr.push(arr[i + 1]);
+        transformer.push(arr[i + 1]);
       }
     } else if (arr[i] === '--double-prev') {
       if (i > 0 && arr[i - 2] !== '--discard-next') {
-        transformedArr.push(arr[i - 1]);
+        transformer.push(arr[i - 1]);
       }
     } else {
-      transformedArr.push(arr[i]);
+      transformer.push(arr[i]);
     }
   }
 
-  return transformedArr;
+  return transformer;
 }
 
 module.exports = {
