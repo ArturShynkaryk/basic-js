1485d2cf35a49185f7496556e01dd74729f5cc88
Artur Shynkaryk <mun9je@gmail.com>
2023-04-13 18:36:42 +0300

    feat: add new tasks

--- src/what-season.js
+++ src/what-season.js
@@ -11,35 +11,81 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
+// function getSeason(date) {
+//   let mounth = date.toString().split(' ');
+//   let season = 'Unable to determine the time of year!';
+//   switch (mounth[1]) {
+//     case 'Dec':
+//     case 'Jan':
+//     case 'Feb':
+//       season = 'winter';
+//       break;
+//     case 'Mar':
+//     case 'Apr':
+//     case 'May':
+//       season = 'spring';
+//       break;
+//     case 'Jun':
+//     case 'Jul':
+//     case 'Aug':
+//       season = 'summer';
+//       break;
+//     case 'Sep':
+//     case 'Oct':
+//     case 'Nov':
+//       season = 'fall';
+//       break;
+//     default:
+//       season = 'Invalid date!';
+//       break;
+//   }
+//   return season;
+// }
+
 function getSeason(date) {
-  let mounth = date.toString().split(' ');
-  let season = 'Unable to determine the time of year!';
-  switch (mounth[1]) {
-    case 'Dec':
-    case 'Jan':
-    case 'Feb':
-      season = 'winter';
-      break;
-    case 'Mar':
-    case 'Apr':
-    case 'May':
-      season = 'spring';
-      break;
-    case 'Jun':
-    case 'Jul':
-    case 'Aug':
-      season = 'summer';
-      break;
-    case 'Sep':
-    case 'Oct':
-    case 'Nov':
-      season = 'fall';
-      break;
-    default:
-      season = 'Invalid date!';
-      break;
+  //     const dateObject = new Date(date);
+
+  // if(dateObject.toString() === 'Invalid Date') {
+  //     console.log('Invalid date string');
+  // } else {
+  //     console.log('Valid date string');
+  // }
+
+  let timestamp = Date.parse(date);
+  if (isNaN(timestamp)) {
+    let season = 'Unable to determine the time of year!';
+    console.log('Invalid date format');
+  } else {
+    console.log('Valid date format');
+    let mounth = date.toString().split(' ');
+
+    switch (mounth[1]) {
+      case 'Dec':
+      case 'Jan':
+      case 'Feb':
+        season = 'winter';
+        break;
+      case 'Mar':
+      case 'Apr':
+      case 'May':
+        season = 'spring';
+        break;
+      case 'Jun':
+      case 'Jul':
+      case 'Aug':
+        season = 'summer';
+        break;
+      case 'Sep':
+      case 'Oct':
+      case 'Nov':
+        season = 'fall';
+        break;
+      default:
+        season = 'Invalid date!';
+        break;
+    }
+    return season;
   }
-  return season;
 }
 
 module.exports = {
